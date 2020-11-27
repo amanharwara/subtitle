e19ea66288b181d9b257fba1f17fae3350430622
Aman Harwara <amanharwara@protonmail.com>
2020-11-27 15:49:04 +0530

    Separate into modules & add opensubs support

--- src/lib.rs
+++ src/lib.rs
@@ -1,100 +1,108 @@
+mod config;
+pub mod error;
+mod opensubs;
 mod subdb;
-use clap::ArgMatches;
-use std::{fs, io, path::Path};
-use subdb::{download, hash, search};
+mod utils;
 
-pub fn run(matches: ArgMatches) -> Result<(), reqwest::Error> {
-    let mut current_api = "subdb";
+use clap::ArgMatches;
+use config::Config;
+use dialoguer::{Confirm, Input, Password};
+use error::{Error, Result};
+use opensubs::{get_os_token, use_opensubs};
+use std::fs;
+use subdb::use_subdb;
+
+pub fn run(matches: ArgMatches) -> Result<()> {
+    let config = Config::new()?;
+
+    let mut current_api: &str = &config.api;
+    let mut current_lang: &str = &config.lang;
+    let mut dont_use_lang_fallback = config.dont_use_lang_fallback;
+    let os_token: &str = &config.os_token;
+
     if let Some(api) = matches.value_of("api") {
         current_api = api;
     };
 
     println!("Current API: {}", current_api);
 
-    let mut current_lang = "en";
     if let Some(lang) = matches.value_of("lang") {
         current_lang = lang;
     }
 
     println!("Current Language: {}", current_lang.to_uppercase());
 
-    match current_api {
-        "subdb" => match matches.values_of("file") {
-            Some(files) => {
-                let files: Vec<&str> = files.collect();
-                for file in files.iter() {
-                    println!("Generating hash for file {}", file);
+    if matches.is_present("dont_use_lang_fallback") {
+        dont_use_lang_fallback = true;
+        println!("\nWon't fallback to English subtitles.");
+    }
 
-                    let hash = hash(file);
-                    println!("Hash generated: {}", hash);
-
-                    let search = search(&*hash)?;
-                    let languages: Vec<&str> = search.split(",").collect();
-
-                    println!("Available Languages: {:?}", languages);
-
-                    // Replace extension of file to .srt
-                    let ext = Path::new(file).extension().unwrap().to_str().unwrap();
-                    let filename = file.to_string().replace(ext, "srt");
-
-                    let downloaded;
-
-                    if languages.len() > 0 {
-                        if languages.contains(&current_lang) {
-                            downloaded = download(&*hash, current_lang)?;
-                            match save_file(&downloaded, filename) {
-                                Ok(_) => println!("File saved!"),
-                                Err(err) => {
-                                    println!("Error while saving file: {}", err);
-                                }
-                            }
-                        } else {
-                            if languages.contains(&"en") {
-                                println!("Could not find a subtitle in the selected language. Downloading the English subtitles.");
-                                downloaded = download(&*hash, "en")?;
-                                match save_file(&downloaded, filename) {
-                                    Ok(_) => println!("File saved!"),
-                                    Err(err) => {
-                                        println!("Error while saving file: {}", err);
-                                    }
-                                }
-                            } else {
-                                println!("Could not find a subtitle in the selected language. You can try again by changing the language using the --lang or -l flag.");
-                            }
-                        }
-                    } else {
-                        println!("No subtitle found for the given file.");
-                        return Ok(());
-                    }
+    match matches.values_of("file") {
+        Some(files) => match current_api {
+            "subdb" => use_subdb(files, current_lang, dont_use_lang_fallback)?,
+            "opensubtitles" => {
+                if os_token.len() > 0 {
+                    use_opensubs(files, current_lang, os_token)?
+                } else {
+                    println!("\nNo OpenSubtitles token found.");
+                    authenticate_os_user()?;
                 }
             }
-            None => {}
+            _ => {}
         },
-        "opensubtitles" => todo!(),
-        _ => {}
+        None => {
+            println!("No files provided.");
+        }
     }
 
     Ok(())
 }
 
-fn save_file(content: &str, filename: String) -> io::Result<()> {
+pub fn authenticate_os_user() -> Result<()> {
+    let mut config = Config::new()?;
+
+    if Confirm::new()
+        .with_prompt("Do you have an OpenSubtitles token?")
+        .interact()?
+    {
+        let token: String = Input::new()
+            .with_prompt("Input your token")
+            .interact_text()?;
+        config.set_os_token(token)?
+    } else {
+        println!("\nYou can generate a token with your username & password.");
+        let username: String = Input::new().with_prompt("Username").interact_text()?;
+        let password: String = Password::new().with_prompt("Password").interact()?;
+        let token = get_os_token(&username, &password)?;
+        config.set_os_token(token)?
+    }
+
+    Ok(())
+}
+
+pub fn save_file(content: &str, filename: &str) -> Result<()> {
     println!("Saving to {}", filename);
-    fs::write(filename, content)
+    fs::write(filename, content).map_err(|e| Error::IO(e))
 }
 
 #[cfg(test)]
 mod tests {
-    use super::subdb::hash;
+    use super::opensubs::hash as opensubs_hash;
+    use super::subdb::hash as subdb_hash;
 
     #[test]
     fn test_hash_fn() {
         assert_eq!(
-            &*hash("./test_videos/dexter.mp4"),
+            &*subdb_hash("./test_videos/subdb/dexter.mp4"),
             "ffd8d4aa68033dc03d1c8ef373b9028c"
         );
         assert_eq!(
-            &*hash("./test_videos/justified.mp4"),
+            &*subdb_hash("./test_videos/subdb/justified.mp4"),
             "edc1981d6459c6111fe36205b4aff6c2"
+        );
+        assert_eq!(
+            &opensubs_hash("./test_videos/opensubs/breakdance.avi").unwrap(),
+            "8e245d9679d31e12"
         );
     }
 }
