9ceed95adccc292acf2155b1d94766ce5f1b90c2
Aman Harwara <amanharwara@protonmail.com>
2020-11-27 15:47:34 +0530

    Update main.rs

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,17 @@
 use std::env;
 extern crate reqwest;
 use clap::{App, Arg};
-use subtitle::run;
-fn main() -> Result<(), reqwest::Error> {
+use subtitle::{error::Result, run};
+
+fn main() -> Result<()> {
     let matches = App::new(env!("CARGO_PKG_NAME"))
         .version(env!("CARGO_PKG_VERSION"))
         .about(env!("CARGO_PKG_DESCRIPTION"))
         .arg(
             Arg::new("file")
                 .index(1)
-                .value_name("FILE")
-                .about("Get subtitles using a file.")
+                .value_name("FILE(S)")
+                .about("Get subtitles for a file or multiple files.")
                 .takes_value(true)
                 .multiple(true),
         )
@@ -32,6 +33,49 @@ fn main() -> Result<(), reqwest::Error> {
                 .takes_value(true)
                 .default_value("en"),
         )
+        .arg(
+            Arg::new("dont_use_lang_fallback")
+                .long("dont-use-lang-fallback")
+                .short('d')
+                .value_name("VALUE")
+                .takes_value(false)
+                .about(
+                    "When used, app will stop if subtitles aren't found for the selected language.",
+                ),
+        )
+        // Commented out subcommand because there's no documentation on how to use it.
+        /* .subcommand(
+            App::new("config")
+                .about("Modify the configuration")
+                .subcommand(
+                    App::new("set")
+                        .about("Set a value in the config.")
+                        .arg(
+                            Arg::new("key")
+                                .index(1)
+                                .value_name("KEY")
+                                .takes_value(true)
+                                .about("The key in the config to change."),
+                        )
+                        .arg(
+                            Arg::new("value")
+                                .index(2)
+                                .value_name("VALUE")
+                                .takes_value(true)
+                                .about("The value to set for the given key."),
+                        ),
+                )
+                .subcommand(
+                    App::new("file").about("Set a new file as the config.").arg(
+                        Arg::new("conf_file")
+                            .index(1)
+                            .value_name("FILE")
+                            .takes_value(true)
+                            .about("The file to set as the new config."),
+                    ),
+                )
+                .subcommand(App::new("list").about("List the complete configuration")),
+        ) */
         .get_matches();
 
     run(matches)
